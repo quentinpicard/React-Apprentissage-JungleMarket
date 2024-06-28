ba414ef9c1c78b1a064edb89c678b6c79d1b11b8
Picard Quentin <quentinp27190@hotmail.fr>
2024-06-28 18:10:40 +0200

    creation dossier style & banner.css

--- maison-jungle/src/composants/Banner.js
+++ maison-jungle/src/composants/Banner.js
@@ -1,5 +1,12 @@
+import '../style/Banner.css';
+
 function Banner(){
-    return <h1>La maison jungle</h1>
+    return (
+        <div className="lmj-banner">
+            <h1>La maison jungle</h1>
+        </div>
+        
+    )
 }
 export default Banner;
 
