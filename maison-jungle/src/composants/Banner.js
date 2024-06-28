25d976e1228a7b7e51006af2932f5d3f2decf49a
Picard Quentin <quentinp27190@hotmail.fr>
2024-06-28 18:25:11 +0200

    Ajout du dossier asset

--- maison-jungle/src/composants/Banner.js
+++ maison-jungle/src/composants/Banner.js
@@ -1,9 +1,14 @@
 import '../style/Banner.css';
+import logo from'../asset/logo.png';
 
 function Banner(){
+    const title = "La maison jungle";
     return (
         <div className="lmj-banner">
-            <h1>La maison jungle</h1>
+            <div className='lmj-title-logo'>
+                <img src={logo} alt='logo de la maison jgl' className='lmj-logo'></img>
+                <h1 className='lmj-title'>{title}</h1>
+            </div>
         </div>
         
     )
