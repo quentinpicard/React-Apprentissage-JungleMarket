533def3bf9e440ed55596e8b76d51b2c0c5ea3bb
Picard Quentin <quentinp27190@hotmail.fr>
2024-06-28 18:57:09 +0200

    ajout fichier cart.css et creation style pour composants cart

--- maison-jungle/src/composants/Cart.js
+++ maison-jungle/src/composants/Cart.js
@@ -1,16 +1,23 @@
+import '../style/Cart.css';
+
 function Cart () {
     const monstera = 8;
     const lierre = 10;
     const bouquetDeFleurs = 15;
     return (
-        <div>
-            <h2>Panier</h2>
-            <ul>
+        <div className="lmj-panier">
+            <div className="lmj-panier-title">
+                <h2>Panier</h2>
+            </div>
+            <ul className="lmj-panier-liste">
                 <li>Monstera : {monstera} €</li>
                 <li>Lierre : {lierre} €</li>
                 <li>Bouquet de Fleur : {bouquetDeFleurs} €</li>
             </ul>
-            <p>Total : {monstera + lierre + bouquetDeFleurs} €</p>
+            <div className="lmj-panier-total">
+                <p>Total</p>
+                <p>{monstera + lierre + bouquetDeFleurs} €</p>
+            </div>
         </div>
     );
 }
