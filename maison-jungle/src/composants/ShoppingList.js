6b138c117e025e489c344e19a5ddbebb7907f6a0
Picard Quentin <quentinp27190@hotmail.fr>
2024-06-28 22:55:57 +0200

    Création fichier SHoppingList.css / style carts plants / Récupération donné tableau

--- maison-jungle/src/composants/ShoppingList.js
+++ maison-jungle/src/composants/ShoppingList.js
@@ -1,18 +1,56 @@
-const plantlist = [
-    'monstera',
-    'ficus lyrata',
-    'pothos argenté',
-    'yucca',
-    'palmier'
-];
+import { plantList } from '../data/plantList';
+import '../style/ShoppingList.css'
+
 
 function ShoppingList () {
+    const categories = plantList.reduce( // récupère les valeurs de catégories dans le tableau plantList et on le met dans une boucle reduce()
+        (acc, plant) =>
+            acc.includes(plant.category)? acc:acc.concat(plant.category), [] // on regarde la valeur de category et on le concat dans un tableau vide []
+    )
     return (
-        <ul>
-            {plantlist.map((plant, index) =>  ( 
-                <li key={`${plant}-${index}`}>{ plant }</li>
-            ))}
-        </ul>
+        <div>
+
+            <div className='lmg-type-categories'>
+                <p>Nos catégories de plante </p>
+                {categories.map((cat) => (
+                    <div key='cat'>{cat}</div>
+                ))}
+            </div>
+            <div className='lmj-all-plant'>
+
+                {plantList.map((plant) =>(
+                    <div key={plant.id} className='lmj-plant'>
+                        <div>{plant.name}</div>
+                        <div>{plant.category}</div>
+                        <div>{plant.isBestSale? <p>🔥</p> : null} </div>
+                        <div>{plant.isSpecialOffer? <p>-20%</p> : null}</div>
+                            
+                    
+                    </div>
+                ))}
+            </div>
+        
+
+
+            {/* <div>
+                <ul>
+                    {categories.map((cat) => (
+                        <li key={cat}>{cat}</li>
+                    ))}
+                </ul>
+                <ul>
+                    {plantList.map((plant) =>  ( 
+                        <li key={plant.id}> { plant.name } { plant.isBestSale? <span>🔥</span> : null} { plant.isSpecialOffer? <span> -20%</span> : null}</li>
+                    ))}
+                </ul>
+                <ul>
+                    {plantList.map((plant) =>  ( 
+                        <li key={plant.id}>{ plant.name }</li>
+                    ))}
+                </ul>
+            </div> */}
+            
+        </div>
     )
 };
 
--- maison-jungle/src/data/plantList.js
+++ maison-jungle/src/data/plantList.js
@@ -2,46 +2,65 @@ export const plantList = [
 	{
 		name: 'monstera',
 		category: 'classique',
-		id: '1ed'
+		id: '1ed',
+		isBestSale: true,
+		isSpecialOffer : true
+		
 	},
 	{
 		name: 'ficus lyrata',
 		category: 'classique',
-		id: '2ab'
+		id: '2ab',
+		isBestSale: false,
+		isSpecialOffer : true
 	},
 	{
 		name: 'pothos argenté',
 		category: 'classique',
-		id: '3sd'
+		id: '3sd',
+		isBestSale: false,
+		isSpecialOffer : false
 	},
 	{
 		name: 'yucca',
 		category: 'classique',
-		id: '4kk'
+		id: '4kk',
+		isBestSale: false,
+		isSpecialOffer : false
 	},
 	{
 		name: 'olivier',
 		category: 'extérieur',
-		id: '5pl'
+		id: '5pl',
+		isBestSale: false,
+		isSpecialOffer : true
 	},
 	{
 		name: 'géranium',
 		category: 'extérieur',
-		id: '6uo'
+		id: '6uo',
+		isBestSale: false,
+		isSpecialOffer : false
 	},
 	{
 		name: 'basilique',
 		category: 'extérieur',
-		id: '7ie'
+		id: '7ie',
+		isBestSale: false,
+		isSpecialOffer : false
 	},
 	{
 		name: 'aloe',
 		category: 'plante grasse',
-		id: '8fp'
+		id: '8fp',
+		isBestSale: false,
+		isSpecialOffer : false
 	},
 	{
 		name: 'succulente',
 		category: 'plante grasse',
-		id: '9vn'
+		id: '9vn',
+		isBestSale: false,
+		isSpecialOffer : false
 	}
 ]
