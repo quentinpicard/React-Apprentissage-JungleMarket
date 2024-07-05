3a2adb11dfcb091dc27f5d85a817fb553d075ad7
Picard Quentin <quentinp27190@hotmail.fr>
2024-07-05 16:54:57 +0200

    commentaire

--- maison-jungle/src/composants/Categories.js
+++ maison-jungle/src/composants/Categories.js
@@ -1,27 +1,37 @@
+// Importation de la liste des plantes depuis un fichier local
 import { plantList } from '../data/plantList';
-import '../style/ShoppingList.css'
 
+// Importation du fichier CSS pour appliquer les styles
+import '../style/ShoppingList.css';
 
-function Categories ({Selectedcatego, setActiveCategory, categories}) {
-
+// Définition du composant fonctionnel Categories
+function Categories ({ Selectedcatego, setActiveCategory, categories }) {
     return (
         <div className='lmg-type-categories'>
-                <div className='lmj-catego'>
-                    <div className='lmj-cat'>
-                        <label for="fruits">Choisissez votre catégories de plantes :</label>
-                        <br />
-                        <select id='plantCategories' value={Selectedcatego} onChange={(e) => setActiveCategory(e.target.value)}>
-                            {categories.map((cat) => (
-                                <option key='cat' value={cat}>
-                                        {cat}
-
-                                </option>
-                            ))}
-                        </select>
-                        <button onClick={()=> setActiveCategory('')} > Effacer le filtre</button>
-                    </div>
+            {/* Conteneur principal pour les catégories */}
+            <div className='lmj-catego'>
+                <div className='lmj-cat'>
+                    {/* Étiquette pour le menu déroulant */}
+                    <label htmlFor="fruits">Choisissez votre catégorie de plantes :</label>
+                    <br />
+                    {/* Menu déroulant pour sélectionner une catégorie */}
+                    <select id='plantCategories'  value={Selectedcatego} onChange={(e) => setActiveCategory(e.target.value)}>
+                        {/* Génération des options du menu déroulant à partir des catégories */}
+                        {categories.map((cat) => (
+                            <option key={cat} value={cat}>
+                                {cat}
+                            </option>
+                        ))}
+                    </select>
+                    {/* Bouton pour effacer le filtre de catégorie */}
+                    <button onClick={() => setActiveCategory('')}>
+                        Effacer le filtre
+                    </button>
                 </div>
             </div>
+        </div>
     )
 }
+
+// Exportation du composant pour pouvoir l'utiliser dans d'autres fichiers
 export default Categories;
